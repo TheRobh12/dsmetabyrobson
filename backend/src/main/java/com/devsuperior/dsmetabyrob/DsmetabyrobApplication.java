8f76a693f979a85e87106cebe8c7be872c48f57a
Robson <robsonbastossantos@gmail.com>
2022-11-15 01:36:41 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmetabyrob/DsmetabyrobApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmetabyrob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmetabyrobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmetabyrobApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmetabyrob/DsmetabyrobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmetabyrob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetabyrobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
